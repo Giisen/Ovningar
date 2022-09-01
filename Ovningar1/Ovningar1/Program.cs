82ef26351fe85443de976e8b420dfdd49e9a7159
giisen <kristerekelund@me.com>
2022-09-01 09:32:59 +0200

    1

---
+++ Ovningar1/Ovningar1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Från genomgången den 2022-09-01, Niklas visar på distans.
+
+int heltal = 0; //Variabeln är deklarerad (int) och initierad (0)
+char tecken = 't'; // endast ett tecken
+double decimaltal = 0.5; //Viktigt att det är en punkt mellan
+string text = "min text";
+
+if (heltal == decimaltal)
+{
+    Console.WriteLine("De är samma!");
+}
+else
+{
+    Console.WriteLine("Dom är inte samma!");
+}
