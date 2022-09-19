eb28c03f8df43280e911b51c0df84889d0fcc462
giisen <kristerekelund@me.com>
2022-09-19 09:50:32 +0200

    jobbar med int[]  foreach

--- Ovningar1/Övningar 23-25/Program.cs
+++ Ovningar1/Övningar 23-25/Program.cs
@@ -1,7 +1,41 @@
 ﻿//23. Skapa en metod int[] IndexOfAll(string text, char c)
 //som söker igenom strängen text och returnerar en int[] med index till alla förekomster av c i text.
 
-int[] IndexOfAll(string text, char c)
+Console.WriteLine("Skriv in en text");
+string text =Console.ReadLine();
+char c = 'c';
+//int index =;
+int result =-1;
+IndexOfAll(text, c);
+
+//int[] ints = new int[text.Length] {IndexOfAll()};
+
+void IndexOfAll(string text, char c)
 {
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == c)
+        {
+            
+            result =i++;
+            int[] index = new int[] {result};
+
+
+            //string stringResult = result.ToString();
+            //stringResult.ToArray();
+            //int[] index=new int[i] {i};
+            //return i;
+            //Console.WriteLine(result); //Denna funkar
+        }
+        //Console.WriteLine(text[i]);
+    }
+
+    foreach(int i in index)
+    {
+        Console.WriteLine(i);
+    }
 
 }
+
+
+
