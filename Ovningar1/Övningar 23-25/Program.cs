77aa9eb8cc2b6098280c797983a42ebeeab6b45a
giisen <kristerekelund@me.com>
2022-09-19 08:39:35 +0200

    övning 23

---
+++ Ovningar1/Övningar 23-25/Program.cs
@@ -0,0 +1,7 @@
+﻿//23. Skapa en metod int[] IndexOfAll(string text, char c)
+//som söker igenom strängen text och returnerar en int[] med index till alla förekomster av c i text.
+
+int[] IndexOfAll(string text, char c)
+{
+
+}
