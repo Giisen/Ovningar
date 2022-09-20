c3dc24ee38b2b4ba28798c1c0aed891444aee962
giisen <kristerekelund@me.com>
2022-09-20 13:50:48 +0200

    24 ej klar, men börjar på 25

--- Ovningar1/Övningar 23-25/Program.cs
+++ Ovningar1/Övningar 23-25/Program.cs
@@ -463,8 +463,8 @@ for (int j = 0; j < h; j++)
 }
 */
 
-/*
-k.
+/*-----------------------------------------------------------EJ KLAR!!!!--------------------------------------------   
+k.                    
 ##--##--##--##--
 ##--##--##--##--
 wwwwww~~~~~~~~~~
@@ -551,3 +551,12 @@ for (int j = 0; j < h; j++)
 
     Console.WriteLine();
 }
+*/
+
+
+//25.Skriv en ny klass som motsvarar en bil. Den ska ha privata fields för modell, pris och färg.
+//Skapa publika properties för att hämta eller ändra värdet på varje field.
+//Skriv en konstruktor till bilklassen som inte tar några parametrar.
+//Skriv en till konstruktor som tar en parameter för varje property som klassen har.
+//Hur kan man styra vilken konstruktor som anropas när man skapar ett objekt av klassen?
+//Skriv en metod till bilklassen med namnet HalfPrice. När den anropas ska priset på bilen sänkas till hälften.
