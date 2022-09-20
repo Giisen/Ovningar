758f6f2a712ad3bfe5f1c9e70683ba75d7ff5cb4
giisen <kristerekelund@me.com>
2022-09-20 17:28:04 +0200

    övning 27 klar

--- Ovningar1/Övningar 26-28/Program.cs
+++ Ovningar1/Övningar 26-28/Program.cs
@@ -2,7 +2,7 @@
 //en metod Step() som räknar upp Steps med 1 varje gång man anropar den;
 //och en metod Reset() som nollställer räknaren.
 //Instantiera klassen och skriv en loop som motsvarar att man går 1000 steg. Skriv ut värdet på Steps.
-
+/*
 StepCount mySteps = new StepCount();
 
 for(int i=0; i<1000; i++)
@@ -31,4 +31,80 @@ public int Step()
         return Steps++;
     }
 
+}
+*/
+
+//27.Skapa en en klass som har en property “red” och en property “blue”. Båda ska vara en float och kunna ha ett värde mellan 0.0 och 100.0
+//men de ska vara “sammankopplade” på så vis att värdena tillsammans alltid är 100.0
+//d.v.s om man t.ex. sätter “blue” till 8.5 och sedan läser av “red” så ska den returnera 91.5
+
+
+
+Console.WriteLine("Välj 1 för att ange värde för Red, välj 2 för att ange värde för Blue:");
+string val = Console.ReadLine();
+
+float Blue = 0.0F;
+float Red = 0.0F;
+
+switch(val)
+{
+    case "1":
+        {
+            Console.WriteLine("Skriv in Red:");
+            string redString = Console.ReadLine();
+            Red = float.Parse(redString);
+            break;
+        }
+
+    case "2":
+        {
+            Console.WriteLine("Skriv in Blue:");
+            string blueString = Console.ReadLine();
+            Blue = float.Parse(blueString);
+            break;
+        }
+}
+
+//Console.WriteLine("Skriv in Red:");
+//string redString = Console.ReadLine();
+//float Red = float.Parse(redString);
+//Console.WriteLine("Skriv in Blue:");
+//string blueString = Console.ReadLine();
+//float Blue = float.Parse(blueString);
+
+
+GetHundred test = new GetHundred();
+test.Blue = Blue;
+test.Red = Red;
+test.ReturnHundred(Red,Blue);
+
+public class GetHundred
+{
+    private float _red;
+    private float _blue;
+
+    public float Red { get { return _red; } set { _red = value; } }
+    public float Blue { get { return _blue; } set { _blue = value; } }
+
+    public void ReturnHundred(float red, float blue)
+    {
+        //float hundred = 100.0F;
+        //float tempRed = hundred-Blue;
+        //float tempBlue = hundred-Red;
+
+        if (Red > 0)
+        {
+            Blue = 100.0F - Red;
+            Console.WriteLine($"Blue är: {Blue}");
+            //return _blue;
+        }
+        else //if (Blue > 0)
+        {
+            Red = 100.0F - Blue;
+            Console.WriteLine($"Red är: {Red}");
+            //return _red;
+        }
+
+        
+    }    
 }
