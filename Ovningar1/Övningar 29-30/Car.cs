74a2f475c722db13335863bbd138880350db0f10
giisen <kristerekelund@me.com>
2022-09-23 10:19:27 +0200

    Halva 29 klar

--- Ovningar1/Övningar 29-30/Car.cs
+++ Ovningar1/Övningar 29-30/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,53 +9,63 @@ namespace Övningar_29_30
 {
     public class Car
     {
-        private int _color;
-        public int Color
+        private ColorEnum _color;
+
+        public ColorEnum Color
         {
+
             get { return _color; }
             set { _color = value; }
         }
 
-        private int _lenght;
-        public int Lenght
+        private LengthEnum _length;
+
+        public LengthEnum Length
         {
-            get { return _lenght; }
-            set { _lenght = value; }
+            get { return _length; }
+            set { _length = value; }
         }
 
-      static Array values = Enum.GetValues(typeof(ColorEnum));
-       static Random random = new Random();
-       public int ColorRand = (int)values.GetValue(random.Next(values.Length));
+        public Car()
+        {
+            Color = ColorIntRand;
+            Length = LengthIntRand;
+        }
+
+        // Skapar en random färg från ColorEnum
+        static Array values = Enum.GetValues(typeof(ColorEnum));
+        static Random random = new Random();
+        public ColorEnum ColorIntRand = (ColorEnum)values.GetValue(random.Next(values.Length));
+
+        // Skapar en random längd från LengthEnum
+        static Array values1 = Enum.GetValues(typeof(LengthEnum));
+        static Random random1 = new Random();
+        public LengthEnum LengthIntRand = (LengthEnum)values1.GetValue(random.Next(values1.Length));
+
+
 
         public enum ColorEnum
         {
-            White,
-            Black,
-            Red,
-            Green,
-            Blue,
-            Yellow,
-            Silver,
-            PerlPink,
-            RadientRouege,
+            RubyRed = 0,
+            GrassGreen = 1,
+            BabyBlue = 2,
+            MellowYellow = 3,
+            SpeedySilver = 4,
+            PerlPink = 5,
+            RadiantRouge = 6,
 
         };
 
-        public enum LenghtEnum
-        {
-            Three,
-            ThreeHalf,
-            four,
-            fourHalf,
-            five,
-        };
-
-        public Car(int ColorRand, int lenght)
+        public enum LengthEnum
         {
             
-            Color=ColorRand;
-            Lenght = lenght;
-        }
+            Three= 3,
+            Four = 4,
+            Five = 5,
+        };
+
+
+
 
     }
 }
--- Ovningar1/Övningar 29-30/Program.cs
+++ Ovningar1/Övningar 29-30/Program.cs
@@ -3,9 +3,11 @@
 // När man instansierar en bil så ska den få en random färg, och en random längd (mellan 3 och 5 meter).
 // Instansiera 1000 bilar och spara i en array. Skriv sedan ut den sammanlagda längden av alla gröna bilar.
 
+
 using Övningar_29_30;
 
-Car bil1 = new Car(ColorRand, 3);
 
-Console.WriteLine(bil1.Color);
-Console.WriteLine(bil1.Color);
+
+
+
+
